d1508b243129edb99fc7e52e1d85678f199472bd
zhanglinfeng1997 <2354558125@qq.com>
2018-05-04 10:36:57 +0800

    添加了Question类，加入了分数控制功能、答案判断功能等

--- Laboratory/BlankPage2.xaml.cs
+++ Laboratory/BlankPage2.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,11 +21,103 @@ namespace Laboratory
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
+    ///
+    
     public sealed partial class BlankPage2 : Page
     {
+        int allpoints = 100;
+        Question tempQuestion = new Question();
+        void InitWithQuestion()
+        {
+      //      tempQuestion = new Question();
+            ChooseA.Background = new SolidColorBrush(Colors.AliceBlue);
+            ChooseB.Background = new SolidColorBrush(Colors.AliceBlue);
+            ChooseC.Background = new SolidColorBrush(Colors.AliceBlue);
+            ChooseD.Background = new SolidColorBrush(Colors.AliceBlue);
+      //      ChooseA.Content = "sasass";
+            ChooseA.Content = tempQuestion.ChoiceA;
+            ChooseB.Content = tempQuestion.ChoiceB;
+            ChooseC.Content = tempQuestion.ChoiceC;
+            ChooseD.Content = tempQuestion.ChoiceD;
+            String a = "此题分数：" + Convert.ToString(tempQuestion.Points);
+            PointsNow.Text = a;
+        }
         public BlankPage2()
         {
             this.InitializeComponent();
+        }
+        public Boolean IsRight(string answer)
+        {
+            if (answer == tempQuestion.RightAnswer)
+                return true;
+            else {
+                return false;
+            }
+        }
+        private void ChooseA_Click(object sender, RoutedEventArgs e)
+        {
+            var choose = (Button)sender;
+           Boolean is_right = IsRight(ChooseA.Content as string);
+            if (is_right)
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGray);
+                allpoints = allpoints - tempQuestion.Points;
+                PointsAll.Text = "当前分数：" + Convert.ToString(allpoints);
+            }
+            //InitWithQuestion();
+        }
+
+        private void ChooseB_Click(object sender, RoutedEventArgs e)
+        {
+            var choose = (Button)sender;
+            Boolean is_right = IsRight(ChooseB.Content as string);
+
+            if (is_right)
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGray);
+                allpoints = allpoints - tempQuestion.Points;
+                PointsAll.Text = "当前分数：" + Convert.ToString(allpoints);
+            }
+        }
+
+        private void ChooseC_Click(object sender, RoutedEventArgs e)
+        {
+            var choose = (Button)sender;
+            Boolean is_right = IsRight(ChooseC.Content as string);
+            if (is_right)
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGray);
+                allpoints = allpoints - tempQuestion.Points;
+                PointsAll.Text = "当前分数：" + Convert.ToString(allpoints);
+            }
+        }
+
+        private void ChooseD_Click(object sender, RoutedEventArgs e)
+        {
+            var choose = (Button)sender;
+            Boolean is_right = IsRight(ChooseD.Content as string);
+            if (is_right)
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                choose.Background = new SolidColorBrush(Colors.LightGray);
+                allpoints = allpoints - tempQuestion.Points;
+                PointsAll.Text = "当前分数：" + Convert.ToString(allpoints);
+            }
         }
     }
 }
