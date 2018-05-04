ca214d5d43d1da823a2020859b760bcaca75e5c2
zhanglinfeng1997 <2354558125@qq.com>
2018-05-04 19:45:39 +0800

    增加了问题的初始化，扣分的全局接口

--- Laboratory/Question.cs
+++ Laboratory/Question.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace Laboratory
 {
+ 
     class Question
     {
         public String questionDescription = "问题描述";
         public String ChoiceA = "选项A";
-        public String ChoiceB = "选项B";
+        public String ChoiceB = "选项B";  
         public String ChoiceC = "选项C";
         public String ChoiceD = "选项D";
         public String RightAnswer = "正确答案";
--- Laboratory/QuestionControl.xaml.cs
+++ Laboratory/QuestionControl.xaml.cs
@@ -14,37 +14,48 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
 
 namespace Laboratory
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
-    ///
-    
-    public sealed partial class BlankPage2 : Page
+    internal delegate void QuestionEventHandler();
+
+    public sealed partial class QuestionControl : UserControl
     {
+        internal event QuestionEventHandler AnswerCorrectly;
+
+        public QuestionControl()
+        {
+
+            this.InitializeComponent();
+            initQuestion();
+            InitWithQuestion();
+        }
         int allpoints = 100;
         int index = 0;
         Question tempQuestion = new Question();
-        List<Question> questionList;
+        List<Question> questionList = new List<Question>();
+        public void decreasePoint(int point)
+        {
+            allpoints -= point;
+            PointsAll.Text = "当前分数：" + Convert.ToString(allpoints);
+        }
         void initQuestion()
         {
-            Question q1 = new Question("试管应如何放置？","水平向左下倾斜","水平向右下倾斜","完全水平放置","完全竖直放置",
-                "水平向左下倾斜",20);
+            Question q1 = new Question("试管应如何放置？", "水平向左下倾斜", "水平向右下倾斜", "完全水平放置", "完全竖直放置",
+                "水平向左下倾斜", 20);
             Question q2 = new Question("如何使用酒精灯加热？", "直接加热", "先进行预热", "以上二者都可行", "以上二者均不可行",
                 "先进行预热", 20);
             Question q3 = new Question("关于收集气体的说法中，正确的是？", "从开始有气泡就收集气体", "等气泡产生速度稳定后收集", "以上二者均可行", "以上二者军不可行",
                 "等气泡产生速度稳定后收集", 10);
             Question q4 = new Question("熄灭酒精灯之前应做什么？", "先将收集的气体密封", "先将导管从液体出拿出", "先把UWP作业写完", "先洗手",
-                "先将导管从液体出拿出",10);
+                "先将导管从液体出拿出", 10);
             this.questionList.Add(q1);
             this.questionList.Add(q2);
             this.questionList.Add(q3);
             this.questionList.Add(q4);
         }
-        void InitWithQuestion()
+        public void InitWithQuestion()
         {
             tempQuestion = this.questionList[index];
             index++;
@@ -52,7 +63,7 @@ namespace Laboratory
             ChooseB.Background = new SolidColorBrush(Colors.AliceBlue);
             ChooseC.Background = new SolidColorBrush(Colors.AliceBlue);
             ChooseD.Background = new SolidColorBrush(Colors.AliceBlue);
-      //      ChooseA.Content = "sasass";
+            //      ChooseA.Content = "sasass";
             ChooseA.Content = tempQuestion.ChoiceA;
             ChooseB.Content = tempQuestion.ChoiceB;
             ChooseC.Content = tempQuestion.ChoiceC;
@@ -60,26 +71,24 @@ namespace Laboratory
             String a = "此题分数：" + Convert.ToString(tempQuestion.Points);
             PointsNow.Text = a;
         }
-        public BlankPage2()
-        {
-            this.InitializeComponent();
 
-        }
         public Boolean IsRight(string answer)
         {
             if (answer == tempQuestion.RightAnswer)
                 return true;
-            else {
+            else
+            {
                 return false;
             }
         }
         private void ChooseA_Click(object sender, RoutedEventArgs e)
         {
             var choose = (Button)sender;
-           Boolean is_right = IsRight(ChooseA.Content as string);
+            Boolean is_right = IsRight(ChooseA.Content as string);
             if (is_right)
             {
                 choose.Background = new SolidColorBrush(Colors.LightGreen);
+                AnswerCorrectly();
             }
             else
             {
@@ -140,3 +149,4 @@ namespace Laboratory
         }
     }
 }
+
