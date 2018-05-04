9b7cfed578e2f01f3cbe3a980d87531779e0a52f
pinkido <pinkido@126.com>
2018-05-04 13:55:27 +0800

    Revert "解决了拖放器材。"
    This reverts commit 16816a47df975fa7f52ea853a39b6bdba1283582.

--- Laboratory/Experiment001.xaml.cs
+++ Laboratory/Experiment001.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Input;
@@ -23,16 +22,10 @@ namespace Laboratory
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
     public sealed partial class Experiment001 : Page
-
     {
-        private double originX;         //记录图片原来位置
-        private double originY;         //记录图片原来位置
-        private int next = 0;               //是否全部显示的标记
-
         public Experiment001()
         {
             this.InitializeComponent();
-
         }
 
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
@@ -69,117 +62,6 @@ namespace Laboratory
             O2.Begin();
             
             
-        }
-
-        private void Image_PointerPressed(object sender, PointerRoutedEventArgs e) //按下某一器材的事件
-        {
-            Image i = sender as Image;
-            originX = (double)i.GetValue(Canvas.LeftProperty);
-            originY = (double)i.GetValue(Canvas.TopProperty);
-            
-        }
-
-
-        private void Image_PointerMoved(object sender, PointerRoutedEventArgs e)    //拖动某一器材的事件,器材图片可以被拖动
-        {
-            Image i = sender as Image;
-            var point = e.GetCurrentPoint(i);
-
-
-            if (point.Properties.IsLeftButtonPressed)
-            {
-                double offsetX = (double)i.GetValue(Canvas.LeftProperty);
-                double offsetY = (double)i.GetValue(Canvas.TopProperty);
-                offsetX += point.Position.X - i.ActualWidth / 2;
-                offsetY += point.Position.Y - i.ActualHeight / 2;
-                (sender as Image).SetValue(Canvas.LeftProperty, offsetX);
-                (sender as Image).SetValue(Canvas.TopProperty, offsetY);
-
-            }
-            
-
-            
-        }
-
-        private void Image_PointerReleased(object sender, PointerRoutedEventArgs e) //放开左键的事件,假如对,显示相应器材;假如错,提示
-        {
-            Image i = sender as Image;
-            double top = (double)i.GetValue(Canvas.TopProperty);
-            if (top < 150)  //未拖到区域
-            {
-                (sender as Image).SetValue(Canvas.LeftProperty, originX);
-                (sender as Image).SetValue(Canvas.TopProperty, originY);
-                return;
-            }else
-            {
-                if ((String)i.Tag == "0")           //错误器材
-                {
-                    i.Visibility = Visibility.Collapsed;
-                    TB001.Text = "Wrong! -5";
-                    return;
-                }else               //正确器材
-                {
-                    i.Visibility = Visibility.Collapsed;
-                    switch (i.Tag)
-                    {
-                        case "1":
-                            Alcohol.Visibility = Visibility.Visible;
-                            AlcoholBurner.Visibility = Visibility.Visible;
-                            break;
-                        case "2":
-                            Sink.Visibility = Visibility.Visible;
-                            Water0.Visibility = Visibility.Visible;
-                            break;
-                        case "3":
-                            Pipe.Visibility = Visibility.Visible;
-                            break;
-                        case "4":
-                            Tube.Visibility = Visibility.Visible;
-                            break;
-                        case "5":
-                            Rubber.Visibility = Visibility.Visible;
-                            break;
-                        case "6":
-                            Jar.Visibility = Visibility.Visible;
-                            Water.Visibility = Visibility.Visible;
-                            break;
-                        case "7":
-                            KMnO4.Visibility = Visibility.Visible;
-                            break;
-                    }
-                    next++;
-                }
-            }
-
-
-        }
-
-
-
-        //以下为判断是否显示完毕的异步事件
-        async Task<int> NextSessionAsync()
-        {
-            bool flag = true;
-
-                while (flag)                //死循环,但因为异步不会卡死
-            {
-                await Task.Delay(10);
-                if (next > 6)               //显示完毕则跳出死循环,等待两秒,进入后续步骤
-                {
-                    flag = false;
-                }
-
-              }
-            await Task.Delay(2000);
-            TB001.Visibility = Visibility.Collapsed;
-                ExpArea.SetValue(Canvas.TopProperty, 50);
-
-            return 0;
-        }
-
-        private async void Button_Start(object sender, RoutedEventArgs e)//此按钮事件为输入化学方程式完毕后的"下一步"按钮所需要完成的事件
-        {
-            await NextSessionAsync();
         }
     }
 }
